8d591237fab37b73aeeeebd2126fde74a0b7a6f2
Jacobjoh6 <114565026+Jacobjoh6@users.noreply.github.com>
2023-08-29 14:58:37 +0200

    fsdf

--- Quiztopia/src/App.tsx
+++ Quiztopia/src/App.tsx
@@ -2,17 +2,23 @@ import './App.css'
 // import { useState } from 'react'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 
-import HomePage from './views/HomePage.tsx/HomePage'
+import HomePage from './views/HomePage/HomePage'
+import MapPage from './views/MapPage/MapPage'
+
+const router =  createBrowserRouter([
+  {
+    path:'/',
+    element: <HomePage />
+  },
+  {
+    path:'/map',
+    element: <MapPage />
+  }
+])
 
 function App() {
   // const [count, setCount] = useState(0)
 
-  const router =  createBrowserRouter([
-    {
-      path:'/',
-      element: <HomePage />
-    },
-  ])
 
   return (
     <div className="App">
