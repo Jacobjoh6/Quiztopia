e4492a33f33df07f45fa2c5d77b9d68cbd60d0c1
Jacobjoh6 <114565026+Jacobjoh6@users.noreply.github.com>
2023-09-04 17:59:07 +0200

    quiz baby

--- Quiztopia/src/views/QuizPage/QuizPage.tsx
+++ Quiztopia/src/views/QuizPage/QuizPage.tsx
@@ -1,10 +1,62 @@
+import { log } from 'console'
+import './QuizPage.css'
+import { useState, useEffect } from 'react'
 
+interface AddQuestion{
+    name: string,
+    question: string,
+    answer: string,
+    location:{
+        longitude: string,
+        latitude: string
+    }
+}
 
 function QuizPage() {
 
+    const [question, setQuestion]   = useState<string>('')
+    const [answer, setAnswer]       = useState<string>('')
+    const [userlocation, setUserlocation] = useState<any>('')
+    // const [longitude, setLongitude] = useState<string>('')
+    // const [latitude, setLatitude]   = useState<string>('')
+
+    const handleQuestion = () => {
+        console.log('question:', question,'Answer:', answer);
+        
+    }
+
+    const handleAnswer = () => {
+        
+    }
+
     return(
-        <div>
-            <p>quiz</p>
+        <div className="quiz-container">
+            <header>
+                <h1>Quizmania</h1>
+            </header>
+            <main>
+                <section className="quiz-map"></section>
+                <aside className="quiz-container__btn">
+                    <div className='btn-div'>
+                        <input type="text" 
+                        placeholder="Question" 
+                        className='input' 
+                        value={question} 
+                        onChange={event => setQuestion(event.target.value)}/>
+                        <button className='btn'
+                        onClick={handleQuestion}>Add question</button>
+                    </div>
+                    <div className='btn-div'>
+                        <input type="text" 
+                        placeholder="Answer" 
+                        className='input' 
+                        value={answer} 
+                        onChange={event => setAnswer(event.target.value)}/>
+                        <button className='btn'>Add answer</button>
+                    </div>
+                </aside>
+            </main>
+
         </div>
     )
 }
