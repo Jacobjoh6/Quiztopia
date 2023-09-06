61837bc3f897e778c2c25da1d5520b5daab26bf9
Jacobjoh6 <114565026+Jacobjoh6@users.noreply.github.com>
2023-09-06 21:28:17 +0200

    hehehe

--- Quiztopia/src/views/QuizPage/QuizPage.tsx
+++ Quiztopia/src/views/QuizPage/QuizPage.tsx
@@ -1,28 +1,46 @@
 import './QuizPage.css'
-import { useState } from 'react'
+import { useState, useRef, useEffect } from 'react'
+import mapboxgl, { Map as MapGl } from 'mapbox-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+import { CreateQuiz, AddQuestion, Position, GetQuiz } from '../../interfaces'
 
-interface CreateQuiz {
-    username: string,
-    quizname: string,
-    token?: string
-}
 
-interface AddQuestion{
-    username: string,
-    question: string,
-    answer: string,
-    location:{
-        longitude: string,
-        latitude: string
-    }
-}
+mapboxgl.accessToken = 'pk.eyJ1IjoiamFjb2Jqb2gwOCIsImEiOiJjbGx6M2pqMXYwZTZ5M2NvNzNscm5rZWtuIn0.C2QkVLDoLFvrae_e65EGpg';
 
 function QuizPage() {
 
     const [quizname, setQuizname] = useState<string>('')
     const [question, setQuestion]   = useState<string>('')
     const [answer, setAnswer]       = useState<string>('')
-    const [userlocation, setUserlocation] = useState<any>(null)
+    const [quizData, setQuizData] = useState<any>(null);
+
+    const mapContainer = useRef(null)
+    const mapRef = useRef<MapGl | null>(null)
+    const [lat, setLat] = useState<number>(57.7)
+    const [lng, setLng] = useState<number>(11.89)
+    const [zoom, setZoom] = useState<number>(10)
+
+
+    useEffect(() => {
+        if( mapRef.current || !mapContainer.current ) return
+
+        mapRef.current = new MapGl({
+            container: mapContainer.current,
+            style: 'mapbox://styles/mapbox/streets-v12',
+            center: [lng, lat],
+            zoom: zoom
+        });
+        const map: MapGl = mapRef.current
+
+        map.on('move', () => {
+
+            const position: Position = map.getCenter()
+            setLat(Number(position.lat.toFixed(4)))
+            setLng(Number(position.lng.toFixed(4)))
+            setZoom(map.getZoom());
+        })
+           
+    }, [lat, lng, zoom])
 
     const handleAddQuiz = async () => {
         const url = 'https://fk7zu3f4gj.execute-api.eu-north-1.amazonaws.com/quiz'
@@ -73,8 +91,46 @@ function QuizPage() {
         const response = await fetch(url, settings)
         const data: AddQuestion = await response.json()
         console.log('added question', data);
-        
     }
+
+    // const handleGetQuiz = async () => {
+	// 	const token = localStorage.getItem('token')
+    //     const username = localStorage.getItem('username')
+	// 	const url = 'https://fk7zu3f4gj.execute-api.eu-north-1.amazonaws.com/quiz'
+
+	// 	const settings = {
+	// 		method: 'GET',
+	// 		headers: {
+	// 			'Content-Type': 'application/json',
+    //             'Authorization': `Bearer ${token}`
+	// 		},
+	// 		body: JSON.stringify({
+    //             name: quizname,
+    //             question: question,
+    //             answer: answer,
+    //             location:{
+    //                 longitude: lng,
+    //                 latitude: lat
+    //             }
+                
+    //         })
+	// 	}
+    //     const response = await fetch(url, settings)
+    //     const data: GetQuiz = await response.json()
+    //     console.log(data);
+    //  }   
+
+    const QuizComponent = () => {
+        const [quizData, setQuizData] = useState(null);
+      
+        useEffect(() => {
+          // Fetch data when the component mounts
+          fetch('https://fk7zu3f4gj.execute-api.eu-north-1.amazonaws.com/quiz')
+            .then((response) => response.json())
+            .then((data) => setQuizData(data))
+            .catch((error) => console.error('Error fetching data:', error));
+        }, []);
+
 
     return(
         <div className="quiz-container">
@@ -82,7 +138,9 @@ function QuizPage() {
                 <h1>Quizmania</h1>
             </header>
             <main>
-                <section className="quiz-map"></section>
+                <section className="quiz-map" ref={mapContainer}>
+
+                </section>
                 <aside>
                     <input type="text" value={quizname}    onChange={event => setQuizname(event.target.value)}/>
                     <button onClick={ handleAddQuiz }>Create quiz</button>
@@ -105,9 +163,32 @@ function QuizPage() {
                         value={answer} 
                         onChange={event => setAnswer(event.target.value)}
                         />
-                        {/* <button className='btn'>Add answer</button> */}
                     </div>
                 </aside>
+                <section>
+                {/* <button onClick={handleGetQuiz}>Get quiz</button> */}
+                <h1>Quiz Information</h1>
+      {quizData ? (
+        <>
+          <p>User ID: {quizData.userId}</p>
+          <p>Quiz ID: {quizData.quizId}</p>
+
+          <h2>Questions</h2>
+          <ul>
+            {quizData.questions.map((question, index) => (
+              <li key={index}>
+                <p>Question: {question.question}</p>
+                <p>Answer: {question.answer}</p>
+                <p>Longitude: {question.location.longitude}</p>
+                <p>Latitude: {question.location.latitude}</p>
+              </li>
+            ))}
+          </ul>
+        </>
+      ) : (
+        <p>Loading...</p>
+      )}
+                </section>
             </main>
 
         </div>
